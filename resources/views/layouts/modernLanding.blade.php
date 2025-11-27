4ae59a5f7bf1ad182a2bded02886b5c2930b42f2
danialrzrul <84134001+danialrzrul@users.noreply.github.com>
2025-11-27 16:54:56 +0800

    Fix Logout Button Not Working on Landing Page

--- resources/views/layouts/modernLanding.blade.php
+++ resources/views/layouts/modernLanding.blade.php
@@ -827,15 +827,10 @@
 								<div class="dropdown-divider"></div>
 								
 								<!-- Logout -->
-								<a class="dropdown-item item-logout" href="{{ route('logout') }}"
-								   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+								<a class="dropdown-item item-logout" href="{{ route('logout') }}">
 									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-dual-screen"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19 3a1 1 0 0 1 1 1v15a1 1 0 0 1 -1 1h-5v2a1 1 0 0 1 -1.351 .936l-8 -3a1 1 0 0 1 -.649 -.936v-15a1 1 0 0 1 .212 -.616l.068 -.079l.078 -.072l.066 -.05l.092 -.058l.065 -.033l.1 -.04l.099 -.028l.046 -.01l.108 -.013l.066 -.001zm-5.649 3.064a1 1 0 0 1 .649 .936v11h4v-13h-7.486z" /></svg>
 								   	Log Keluar
 								</a>
-								
-								<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-									@csrf
-								</form>
 							</div>
 						</div>
 					@endif
