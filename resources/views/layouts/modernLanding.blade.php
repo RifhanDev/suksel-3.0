294a0ed4b98289277501e0e38c76778e2f201583
danialrzrul <84134001+danialrzrul@users.noreply.github.com>
2025-12-03 16:45:12 +0800

    add menu to the direktori agensi

--- resources/views/layouts/modernLanding.blade.php
+++ resources/views/layouts/modernLanding.blade.php
@@ -755,8 +755,8 @@
 						</div>
 					</li>
 					<li class="nav-item dropdown">
-						<a href="#navbar-help" 
-						class="nav-link dropdown-toggle {{ request()->is('manuals*') ? 'active' : '' }}" 
+						<a href="#navbar-agencies" 
+						class="nav-link dropdown-toggle" 
 						id="navbarDropdownAgensi"
 						role="button" 
 						data-bs-toggle="dropdown" 
@@ -770,14 +770,17 @@
 							<span>Direktori Agensi</span>
 						</a>
 						<div class="dropdown-menu" aria-labelledby="navbarDropdownAgensi">
-							<a class="dropdown-item" href="{{ action('HelpsController@index') }}">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-lifebuoy"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14.757 16.172l3.571 3.571a10.004 10.004 0 0 1 -12.656 0l3.57 -3.571a5 5 0 0 0 2.758 .828c1.02 0 1.967 -.305 2.757 -.828m-10.5 -10.5l3.571 3.57a5 5 0 0 0 -.828 2.758c0 1.02 .305 1.967 .828 2.757l-3.57 3.572a10 10 0 0 1 -2.258 -6.329l.005 -.324a10 10 0 0 1 2.252 -6.005m17.743 6.329c0 2.343 -.82 4.57 -2.257 6.328l-3.571 -3.57a5 5 0 0 0 .828 -2.758c0 -1.02 -.305 -1.967 -.828 -2.757l3.571 -3.57a10 10 0 0 1 2.257 6.327m-5 -8.66q .707 .41 1.33 .918l-3.573 3.57a5 5 0 0 0 -2.757 -.828c-1.02 0 -1.967 .305 -2.757 .828l-3.573 -3.57a10 10 0 0 1 11.33 -.918" /></svg>
-								Bantuan
-							</a>
-							<a class="dropdown-item" href="{{ route('manuals.show', 'pendaftaran') }}">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-alert-square"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19 2a3 3 0 0 1 2.995 2.824l.005 .176v14a3 3 0 0 1 -2.824 2.995l-.176 .005h-14a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-14a3 3 0 0 1 2.824 -2.995l.176 -.005h14zm-6.99 13l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z" /></svg>
-								Panduan Pengguna
-							</a>
+							@php
+								try {
+								    $__orgTypes = App\OrganizationType::orderBy('sort_no', 'asc')->get();
+								} catch (\Throwable $e) {
+								    $__orgTypes = collect();
+								}
+							@endphp
+							@foreach ($__orgTypes as $type)
+								<a class="dropdown-item"
+									href="{{ action('OrganizationUnitsController@index', ['type' => $type->id]) }}">{{ $type->name }}</a>
+							@endforeach
 						</div>
 					</li>
 				</ul>
