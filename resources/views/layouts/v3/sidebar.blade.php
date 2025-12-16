e74d53303ad8c2c5edc34e7fa881eaf8ef4fb8bd
muddasir_rifhan <muddasir@rifhan.com>
2025-12-16 11:04:29 +0800

    remove keputusan msyuarat

--- resources/views/layouts/v3/sidebar.blade.php
+++ resources/views/layouts/v3/sidebar.blade.php
@@ -468,14 +468,6 @@
 					</div>
 				</li>
 
-				<!-- Menu : Keputusan Mesyuarat -->
-				<li class="nav-item">
-					<a class="sidebar-link {{ request()->is('keputusan-mesyuarat*') ? 'active' : '' }}" href="{{ route('keputusanMesyuarat') }}" style="cursor: pointer;">
-						<svg xmlns="http://www.w3.org/2000/svg" class="nav-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-						<span class="nav-text">Keputusan Mesyuarat</span>
-					</a>
-				</li>
-
 
 			</ul>
 		</div>
