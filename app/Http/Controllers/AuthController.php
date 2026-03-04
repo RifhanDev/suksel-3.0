5ff4b139d897fff5b39607e3ab802e44607b8340
Edry Rifhan <edry@rifhan.com>
2026-03-04 11:39:28 +0800

    up

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -110,8 +110,8 @@ class AuthController extends Controller
 
                // Check if password needs to be changed (6 months expiration)
                if ($user->password_changed_at) {
-                  // password_changed_at is already a Carbon instance due to $dates array
-                  $passwordAge = $user->password_changed_at->diffInMonths(Carbon::now());
+                  $passwordChangedAt = Carbon::parse($user->password_changed_at);
+                  $passwordAge = $passwordChangedAt->diffInMonths(Carbon::now());
                   if ($passwordAge >= 6) {
                      // Password expired, redirect to change password
                      session()->flash('warning', 'Kata laluan anda telah tamat tempoh (6 bulan). Sila tukar kata laluan anda.');
