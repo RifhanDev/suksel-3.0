e0012ac6b85e5f2d135018dbbd9b6cf57df3563b
hazmi-rifhan <hazmi@rifhan.com>
2026-07-08 11:08:43 +0800

    update

--- app/Http/Controllers/JawatankuasaController.php
+++ app/Http/Controllers/JawatankuasaController.php
@@ -430,16 +430,7 @@ class JawatankuasaController extends Controller
             ->whereNotNull('user_id')
             ->update(['dihantar_pemakluman_pada' => Carbon::now()]);
 
-<<<<<<< HEAD
-        $tender->update(['status_process_id' => 2]);
-=======
-        $tender->refresh();
         app(TenderProcessStatusService::class)->markPelantikanJawatankuasaSelesai($tender);
-=======
-        $tender->refresh();
-        app(TenderProcessStatusService::class)->markPelantikanJawatankuasaSelesai($tender);
->>>>>>> ef2addc99f96ee697754b9781a138906b76e3efe
->>>>>>> main
 
         Log::debug('hantarPemakluman completed', [
             'email_count' => $emailCount,
